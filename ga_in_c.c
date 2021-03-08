00ba58e6c508a2349b0862003a8f4a7fe9e6fa7a
gagangayari <55296443+gagangayari@users.noreply.github.com>
2021-03-08 13:10:10 +0530

    Update ga_in_c.c

--- ga_in_c.c
+++ ga_in_c.c
@@ -1,27 +1,64 @@
 #include<bits/stdc++.h>
 
 #define key_size 167
-int get_random_bit(int size){
+#define mut_prob 0.01
+#define xover_prob 0.3
+
+
+/* Generate random key of size as key_size */
+void get_random_bit(struct chrom *c){
+    for(int i=0;i<key_size;i++){
+        c->key[i]=(random()>RAND_MAX/2);
+
+    }
 
 }
+
+
+
 struct chrom
 {
-    int key[key_size]=get_random_bit();
+    int key[key_size];
+    
+    //set random key
+    for(int i=0;i<key_size;i++){
+        key[i]= (random()>RAND_MAX/2);
+
+    }
+    
     int fitness= -1;
-}chrom;
+};
 
 
-void flip_bits(){  //Flip bits at some location
+
+/*Flip bits at some location*/
+void flip_bits(sample,int i,int j){
+    //Exploitation
+    for(int k=16;k<32;k++){
+        if(random()/RAND_MAX < mut_prob){
+            if(sample[i+k]==0){
+                sample[i+k]=1;
+            }
+            else{
+                sample[i+k]=0;
+            }
+
+        }
+        
+
+
+    }
+
 
 }
 void mutation(chrom sample){
     //Mutation in Constants
     //const1 k1-k32
-    flip_bits(sample,0,31)
+    flip_bits(sample,0,31);
     flip_bits(sample,32,63);
     flip_bits(sample,64,95)
-    flip_bits(sample,96,137);
-    flip_bits(sample,138,169);
+    flip_bits(sample,96,127);
+    flip_bits(sample,128,159);
 
     //Mutation for remaining bits
 
@@ -29,12 +66,91 @@ void mutation(chrom sample){
 
 }
 
-void helper_xover_consts(){
+void helper_xover_consts(chrom *mate1,chrom *mate2,int i ,int j){
+    /*i and j are the starting and ending indexes of the constants
+    E.g For constant 1: i=0,j=31
+    */
+    int point=random() % 32;
+    int temp;
+    for(int k=point;k<=j;k++){
+        temp=mate2->key[k];
+        mate2->key[k]=mate2->key[k];
+        mate1->key[k]=temp;
+
+    }
 
 }
-void crossover(chrom mate1,chrom mate2){
-    //for constants
-    mate1.key[]
+void crossover(chrom *mate1,chrom *mate2){
+    /* Method for one-point crossover
+
+    */
+
+    //For constants
+    helper_xover_consts(mate1,mate2,0,31);
+    helper_xover_consts(mate1,mate2,32,63);
+    helper_xover_consts(mate1,mate2,64,95);
+    helper_xover_consts(mate1,mate2,96,127);
+    helper_xover_consts(mate1,mate2,128,159);
+
+
+    //For remaining bits
+
+    int random_point=random()%7;
+    int temp;
+    for(int i=point;i<key_size;i++){
+        temp=mate2->key[i];
+        mate2->key[i]=mate1->key[i];
+        mate1->key[i]=temp;
+
+    }
+    
+
+/*Selection of Parents*/
+
+//We select 10 random samples and pick the two fittest samples
+
+bool check_if_not_picked(int picked_arr[],int index){
+
+
+}
+
+int pick_best(struct chrom samples[],int n){
+
+}
+void selection (struct chrom samples[],int n){//n is the size of the samples array
+    int n_sample=10;
+    int first_set[n_sample],second_set[n_sample];
+    int count=0,i=0,index;
+    //generate  10 random indexes
+    while (count!=10 ){
+       index= random()/n;
+       if( check_if_not_picked(first_set,index) ){
+           first_set[i]=index;
+       
+       }
+       count++;
+       i++;
+
+    }
+    
+
+    
+
+}
+
+
+/* Calculates the fitness value for each samples*/
+
+void fitness(struct chrom samples[],int n){
+    for(int i=0;i<n;i++){
+
+
+    }
+
+
+}
+
+
     
 
 }
